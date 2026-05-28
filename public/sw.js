6198c13fe54972786a90d3dc874e9c7b27a3cf42
st162210 <st162210@stud.uni-stuttgart.de>
2026-05-28 21:21:00 +0200

    fix: Base-Path für GitHub Pages angepasst und Service Worker Cache aktualisiert

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vml-app-shell-v2';
+const CACHE_NAME = 'vml-app-shell-v3';
 const APP_SHELL = ['.', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
